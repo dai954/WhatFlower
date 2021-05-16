1cb9282be4057a21af27a8d634eb92b15ad1b1b9
dai954 <da0606ha@gmail.com>
2021-05-16 13:53:00 +0900

    add_podfile

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 import CoreML
 import Vision
+import Alamofire
+import SwiftyJSON
+import SDWebImage
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var label: UILabel!
+    
+    let wikipediaURl = "https://en.wikipedia.org/w/api.php"
     let imagePicker = UIImagePickerController()
 
     override func viewDidLoad() {
@@ -25,12 +31,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
-            imageView.image = userPickedImage
+            
             
             guard let ciImage = CIImage(image: userPickedImage) else {
                 fatalError("Ciimage coudn't be converted.")
             }
             
+//            imageView.image = userPickedImage
             detect(image: ciImage)
             
         }
@@ -51,10 +58,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             }
             
             self.navigationItem.title = classification.identifier.capitalized
+            self.requestInfo(flowerName: classification.identifier)
 
-//            if let firstresult = results.first {
-//
-//            }
+
             
             
         }
@@ -67,6 +73,53 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             print(error)
         }
         
+    }
+    
+    
+    func requestInfo(flowerName: String) {
+        
+        let parameters : [String:String] = [
+          "format" : "json",
+          "action" : "query",
+          "prop" : "extracts|pageimages",
+          "exintro" : "",
+          "explaintext" : "",
+          "titles" : flowerName,
+          "indexpageids" : "",
+          "redirects" : "1",
+          "pithumsize": "500"
+          ]
+
+        
+        Alamofire.request(wikipediaURl, method: .get, parameters: parameters).responseJSON { (response) in
+            if response.result.isSuccess {
+                print("Got the wikipedia info")
+                print(response)
+                print("-----------------------------")
+                print(JSON(response.result.value))
+                
+                let flowerJSON: JSON = JSON(response.result.value!)
+                let pageid = flowerJSON["query"]["pageids"][0].stringValue
+                let flowerDescription = flowerJSON["query"]["pages"][pageid]["extract"].stringValue
+                self.label.text = flowerDescription
+                let flowerImageURL = flowerJSON["query"]["pages"][pageid]["thumbnail"]["source"].stringValue
+                
+                self.imageView.sd_setImage(with: URL(string: flowerImageURL))
+                
+//                if let flowerImageStringURL = URL(string: flowerImageURL) {
+//                            if let data = try? Data(contentsOf: flowerImageStringURL) {
+//                                if let image = UIImage(data: data) {
+//                                    DispatchQueue.main.async {
+//                                        self.imageView?.image = image
+//                                    }
+//                                }
+//                            }
+//                        }
+                
+            } else {
+                print("Not succeed in wiki api!!")
+            }
+        }
     }
     
 
